dd8574ab55ba9e4221ba596c526f7a263830a561
ParkJiwoo1 <50094118+ParkJiwoo1@users.noreply.github.com>
2020-11-17 16:34:21 +0900

    Add files via upload

---
+++ Frame_make.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Frame_make {
+	public static final int f_width = 800;
+	public static final int f_height = 600;
+	public static void main(String[] args) {
+		
+		
+	new star();
+	}
+	}
+	
+	
